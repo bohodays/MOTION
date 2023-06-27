920ab2f1d663a99a8fd41b20b8b0ed9813452624
bohodays <bohodays0316@gmail.com>
2023-06-27 20:16:28 +0900

    update: youtube

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -13,11 +13,25 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
       ".video__iframe"
     )! as HTMLIFrameElement;
     iframe.src = "https://www.youtube.com/embed/5iZ-igA_b4g"; // url -> videoId -> embed
-    console.log(url);
+
+    iframe.src = this.convertToEmbeddedURL(url);
 
     const titleElement = this.element.querySelector(
       ".video__title"
     )! as HTMLHeadingElement;
     titleElement.textContent = title;
+  }
+
+  // 정규표현식 Regex
+  private convertToEmbeddedURL(url: string): string {
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 }
