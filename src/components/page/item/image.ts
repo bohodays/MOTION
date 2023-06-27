d28cf2283e180281146742e8217fa6743c88c11f
bohodays <bohodays0316@gmail.com>
2023-06-27 16:43:28 +0900

    update: baseComponent

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,18 +1,10 @@
-export class ImageComponent {
-  private element: HTMLElement;
-  constructor(title: string, url: string) {
-    const template = document.createElement("template");
-    // 사용자가 위험한 코드를 바로 동적으로 만들 수 있기 때문에 아래의 코드에 ${title} 형태로 넣지 않는다.
-    template.innerHTML = `
-    <section class="image">
-      <div class="image__holder">
-        <img class="image__thumbnail" />
-      </div>
-      <p class="image__title"></p>
-    </section>
-    `;
+import { BaseComponent } from "../../component.js";
 
-    this.element = template.content.firstElementChild! as HTMLElement; // section 태그를 가리킨다.
+export class ImageComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(
+      '<section class="image"><div class="image__holder"><img class="image__thumbnail" /></div><p class="image__title"></p></section>'
+    );
 
     const imageElement = this.element.querySelector(
       ".image__thumbnail"
@@ -26,7 +18,35 @@ export class ImageComponent {
     titleElement.textContent = title;
   }
 
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin"): void {
-    parent.insertAdjacentElement(position, this.element);
-  }
+  // BaseComponent 만들기 이전 방식
+  // private element: HTMLElement;
+  // constructor(title: string, url: string) {
+  //   const template = document.createElement("template");
+  //   // 사용자가 위험한 코드를 바로 동적으로 만들 수 있기 때문에 아래의 코드에 ${title} 형태로 넣지 않는다.
+  //   template.innerHTML = `
+  //   <section class="image">
+  //     <div class="image__holder">
+  //       <img class="image__thumbnail" />
+  //     </div>
+  //     <p class="image__title"></p>
+  //   </section>
+  //   `;
+
+  //   this.element = template.content.firstElementChild! as HTMLElement; // section 태그를 가리킨다.
+
+  //   const imageElement = this.element.querySelector(
+  //     ".image__thumbnail"
+  //   )! as HTMLImageElement;
+  //   imageElement.src = url;
+  //   imageElement.alt = title;
+
+  //   const titleElement = this.element.querySelector(
+  //     ".image__title"
+  //   )! as HTMLParagraphElement;
+  //   titleElement.textContent = title;
+  // }
+
+  // attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin"): void {
+  //   parent.insertAdjacentElement(position, this.element);
+  // }
 }
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,13 +1,20 @@
-export class PageComponents {
-  private element: HTMLUListElement;
+import { BaseComponent } from "../component.js";
+
+export class PageComponents extends BaseComponent<HTMLUListElement> {
   constructor() {
-    this.element = document.createElement("ul");
-    this.element.setAttribute("class", "page");
-    this.element.textContent = "This is PageComponent";
+    // 무조건 상속하는 자식 클래스에서는 super를 이용해서 부모 클래스의 생성자를 호출해야 된다.
+    super('<ul class="page">This is PageComponent</ul>');
   }
 
-  // position의 타입이 InsertPosition라는 것은 insertAdjacentElement 함수에 대한 설명을 통해 알 수 있다.
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin"): void {
-    parent.insertAdjacentElement(position, this.element);
-  }
+  // BaseComponent 만들기 이전 방식
+  // private element: HTMLUListElement;
+  // constructor() {
+  //   this.element = document.createElement("ul");
+  //   this.element.setAttribute("class", "page");
+  //   this.element.textContent = "This is PageComponent";
+  // }
+  // // position의 타입이 InsertPosition라는 것은 insertAdjacentElement 함수에 대한 설명을 통해 알 수 있다.
+  // attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin"): void {
+  //   parent.insertAdjacentElement(position, this.element);
+  // }
 }
