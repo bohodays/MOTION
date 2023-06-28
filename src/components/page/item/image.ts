2fdad30d116471b9f1c75ebeeecb89ce53c40ac7
bohodays <bohodays0316@gmail.com>
2023-06-28 12:19:39 +0900

    update: style

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -3,7 +3,7 @@ import { BaseComponent } from "../../component.js";
 export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(
-      '<section class="image"><div class="image__holder"><img class="image__thumbnail" /></div><p class="image__title"></p></section>'
+      '<section class="image"><div class="image__holder"><img class="image__thumbnail" /></div><h2 class="page-item__title image__title"></h2></section>'
     );
 
     const imageElement = this.element.querySelector(
